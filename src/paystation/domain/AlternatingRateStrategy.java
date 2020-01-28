8c5b00e097587e95be9f471e3c4f69c162f7766c
Daniel McShane <danielmcshane7@gmail.com>
2020-01-28 14:54:11 -0500

    Added skeletons for different rate classes

---
+++ src/paystation/domain/AlternatingRateStrategy.java
@@ -0,0 +1,13 @@
+package paystation.domain;
+
+public class AlternatingRateStrategy implements RateStrategy{
+
+	@Override
+	public int calculateTime(int moneyInserted) {
+		// TODO Abraham
+		return 0;
+	}
+
+	
+	
+}
---
+++ src/paystation/domain/ProgressiveRateStrategy.java
@@ -0,0 +1,12 @@
+package paystation.domain;
+
+public class ProgressiveRateStrategy implements RateStrategy{
+
+	@Override
+	public int calculateTime(int moneyInserted) {
+		// TODO DANIEL
+		return 0;
+	}
+
+	
+}
