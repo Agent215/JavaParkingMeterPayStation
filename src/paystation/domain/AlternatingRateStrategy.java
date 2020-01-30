17bf73ab549d8520f43c9fbe18be219d17e339a1
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-01-29 21:39:59 -0500

    added UML diagram protypes. will add more methods as we design further

--- src/paystation/domain/AlternatingRateStrategy.java
+++ src/paystation/domain/AlternatingRateStrategy.java
@@ -1,11 +1,14 @@
+/* On Weekdays Uses Progressive Rate  */
+/* On Weekends Uses Linear Rate Strategy */
+
 package paystation.domain;
 
 public class AlternatingRateStrategy implements RateStrategy{
 
 	@Override
-	public double calculateTime(int moneyInserted) {
-		// TODO Abraham
-		return 0;
+	public double calculateTime(int moneyIn) {
+		
+            return 0;
 	}
 
 	
