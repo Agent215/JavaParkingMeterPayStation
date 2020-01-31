29da16759f9a24abe83f15415434ba913572fbcc
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-01-31 14:52:51 -0500

    implemented alterating strategy class. still needs test method

--- src/paystation/domain/AlternatingRateStrategy.java
+++ src/paystation/domain/AlternatingRateStrategy.java
@@ -1,16 +1,38 @@
 /* On Weekdays Uses Progressive Rate  */
-/* On Weekends Uses Linear Rate Strategy */
-
+ /* On Weekends Uses Linear Rate Strategy */
 package paystation.domain;
 
-public class AlternatingRateStrategy implements RateStrategy{
+import java.util.Calendar;
+import java.util.Date;
 
-	@Override
-	public double calculateTime(int moneyIn) {
-		
-            return 0;
-	}
+public class AlternatingRateStrategy implements RateStrategy {
 
-	
-	
+    /* we will have a private rate strategy for the alternating strategy which can switch 
+       as needed */
+    private RateStrategy rs;
+    private Date day;
+    private int moneyInserted;
+    private double time;
+
+    @Override
+    public double calculateTime(int moneyIn) {
+
+        moneyInserted = moneyIn;
+        /*get current day of the week as a number */
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(day);
+        System.out.println("this is the " + calendar.get(Calendar.DAY_OF_WEEK) + "th day of the week");
+
+        /*if the day is saturday or sunday then lets change rate strategy*/
+        if (Calendar.DAY_OF_WEEK == 1 || Calendar.DAY_OF_WEEK == 7) {
+            rs = new LinearRateStrategy();
+            time = rs.calculateTime(moneyInserted);
+        } else {
+            /* else if its the week */
+            rs = new ProgressiveRateStrategy();
+            time = rs.calculateTime(moneyInserted);
+        }
+        return time;
+    }
+
 }
