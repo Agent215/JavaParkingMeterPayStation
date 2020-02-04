f31bcf190a4c1e03f4044323efa68d67ae585251
Daniel McShane <danielmcshane7@gmail.com>
2020-02-04 16:53:31 -0500

    Implemented simulation of adding coins in main method.

--- src/paystation/domain/PayStationMain.java
+++ src/paystation/domain/PayStationMain.java
@@ -12,17 +12,54 @@ package paystation.domain;
 import java.util.Scanner;
 
 public class PayStationMain {
+	static Scanner input = new Scanner(System.in);
+	static PayStation ps;
+	public static void depositCoins() throws IllegalCoinException {
+		if (ps == null) {
+			System.out.println("A rate strategy has not been chosen.");
+			System.out.println("Please choose from the following rate strategies: A for alternating, P for progressive, and L for linear.");
+			input.nextLine();
+			String rateStrategy = input.nextLine();
+			
+			if (rateStrategy.equalsIgnoreCase("A")) 
+				ps = new PayStationImpl(new AlternatingRateStrategy());
+			else if (rateStrategy.equalsIgnoreCase("P"))
+				ps = new PayStationImpl(new ProgressiveRateStrategy());
+			else if (rateStrategy.equalsIgnoreCase("L"))
+				ps = new PayStationImpl();
+			else
+			{
+				System.out.println("Invalid rate strategy. Please try again.");
+				return;
+			}
+		}
+		
+		int coin = -1;
+		while (coin != 0) {
+			System.out.println("Type in the amount you'd like to deposit or 0 to exit. (5, 10, or 25): ");
+			coin = input.nextInt();
+			if (coin == 0)
+				return;
+			try {
+				ps.addPayment(coin);
+				System.out.println("Coin accepted.");
+			}
+			catch (IllegalCoinException e) {
+				System.out.println(e.getMessage());
+			}
+			
+		}
+	}
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalCoinException {
 
         // running flag
         boolean running;
         //instance of paystation
-        PayStation ps;
+        
         // we start in running state
         running = true;
         // providing no argument defaults to linear payrate 
-        ps = new PayStationImpl();
 
         // main while loop for simulation
         while (running == true) {
@@ -32,11 +69,11 @@ public class PayStationMain {
                     + " : cancel\n5 : change rate strategy\n"
                     + "please make a selction by typing a number");
             //get user input as int 
-            Scanner input = new Scanner(System.in);
+            
             int userChoice = input.nextInt();
             switch (userChoice) {
                 case 1:
-                    // code block
+                    depositCoins();
                     break;
                 case 2:
                     // code block
@@ -51,7 +88,8 @@ public class PayStationMain {
                     // code block
                     break;
                 default:
-                System.out.println("you entered an invalid choice option");
+                	System.out.println("you entered an invalid choice option");
+                	running = false;
             }
 
         } // end while
